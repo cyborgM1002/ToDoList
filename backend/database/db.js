dea769c7dea431874c22f82f6f5f2d086563500a
cyborgM1002 <mkchauhan0543@gmail.com>
2023-05-10 14:23:10 +0530

    ready to deploy

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 export const mongoDB = () => {
-  mongoose
-    .connect(process.env.MONGO_URI, { dbName: "ToDoList" })
-    .then(() => {
-      console.log("Database Connected");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+	mongoose
+		.connect(process.env.MONGO_URI, {})
+		.then(() => {
+			console.log("Database Connected");
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 };
